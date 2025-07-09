5cc5a7814ffdacb9c43e4a38c87987bd060b8c1e
SeongRyong Lim <ryongseong.dev@gmail.com>
2025-07-09 17:10:23 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.69 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    cnt, zero_cnt = 0, 0
+
+    while s != "1":
+        cnt += 1
+        num = s.count("1")
+        zero_cnt += len(s) - num
+        s = bin(num)[2:]
+
+    return [cnt, zero_cnt]
