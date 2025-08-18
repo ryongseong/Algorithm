39d6e1564159f1a947c9b02df2f4b6c1309f5f0d
SeongRyong Lim <ryongseong.dev@gmail.com>
2025-08-18 15:47:33 +0900

    [Bronze V] Title: 합, Time: 96 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = 0;
+
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        System.out.println(result);
+    }
+}
