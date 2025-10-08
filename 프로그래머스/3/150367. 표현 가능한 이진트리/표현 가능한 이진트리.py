89acc158547d699576e07c9889e5f8eb7eb61e71
SeongRyong Lim <ryongseong.dev@gmail.com>
2025-10-09 00:27:29 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 155.57 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -0,0 +1,39 @@
+from math import log
+
+
+def check(binary_number, prev_parent):
+    mid = len(binary_number) // 2
+    if binary_number:
+        child = (binary_number[mid]) == "1"
+    else:
+        return True
+
+    if child and not prev_parent:
+        return False
+    else:
+        return check(binary_number[mid + 1 :], child) and check(
+            binary_number[:mid], child
+        )
+
+
+def sol(number):
+    if number == 1:
+        return 1
+    binary_number = bin(number)[2:]
+    digits = 2 ** (int(log(len(binary_number), 2)) + 1) - 1
+    binary_number = "0" * (digits - len(binary_number)) + binary_number
+
+    return (
+        1
+        if binary_number[len(binary_number) // 2] == "1" and check(binary_number, True)
+        else 0
+    )
+
+
+def solution(numbers):
+    answer = []
+
+    for num in numbers:
+        answer.append(sol(num))
+
+    return answer
